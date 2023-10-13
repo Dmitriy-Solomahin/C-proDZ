064d7398ab3812b1137673767d7fc649adb63c54
Dmitriy Solomahin <solomahin_dima@mail.ru>
2023-10-13 20:16:17 +0300

    add Dz2.1

---
+++ Lesson2_1/Lesson2_1/FamilyMember.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_1
+{
+    public enum Gender { 
+        Male,
+        Female
+    }
+    public class FamilyMember{
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+
+        public virtual void Info(int index) {
+            Console.WriteLine($"{new String(' ', index)} -- {this.Name}");
+        }
+        public virtual void Print(int indent = 0)
+        {
+            Info(indent);
+        }
+    }
+
+    public class AdultFamilyMember : FamilyMember
+    {
+        public AdultFamilyMember spouse { get; set; }
+        public List<FamilyMember> children { get; set; } = new List<FamilyMember>();
+
+        public override void Info(int index){
+            Console.WriteLine($"{new String(' ', index)} -- {this.Name} + {spouse.Name}");
+            Console.WriteLine($"{new String(' ', index + 4)} |");
+            Console.WriteLine($"{new String(' ', index + 4)} |");
+        }
+        public override void Print(int indent = 0)
+        {
+            base.Print(indent);
+
+            foreach (var child in children)
+                child.Print(indent + 4);
+        }
+
+        public void AddChildren(FamilyMember child)
+        {
+            children.Add(child);
+            spouse.children.Add(child);
+        }
+
+        public void Wedding(AdultFamilyMember spouse)
+        {
+            this.spouse = spouse;
+            spouse.spouse = this;
+        }
+
+
+
+    }
+}
---
+++ Lesson2_1/Lesson2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var grendFather = new AdultFamilyMember() { Mother = null, Father = null, Name = "Bob", Sex = Gender.Male};
+            var grendMather = new AdultFamilyMember() { Mother = null, Father = null, Name = "Ann", Sex = Gender.Female };
+            var father = new AdultFamilyMember() { Mother = grendMather, Father = grendFather, Name = "Karl", Sex = Gender.Male };
+            var mather = new AdultFamilyMember() { Mother = null, Father = null, Name = "Karla", Sex = Gender.Female };
+            var son1 = new AdultFamilyMember() { Mother = mather, Father = father, Name = "Adam", Sex = Gender.Male };
+            var son2 = new FamilyMember() { Mother = mather, Father = father, Name = "Sem", Sex = Gender.Male };
+            var daughter1 = new FamilyMember() { Mother = mather, Father = father, Name = "Mery",Sex = Gender.Female };
+            var daughter2 = new FamilyMember() { Mother = mather, Father = father, Name = "Samanta", Sex = Gender.Female };
+            var eva = new AdultFamilyMember() { Mother = null, Father = null, Name = "Eva", Sex = Gender.Female };
+            var kain = new FamilyMember() { Mother = son1, Father = eva, Name = "Kain", Sex = Gender.Male };
+
+            grendFather.Wedding(grendMather);
+            father.Wedding(mather);
+            grendFather.AddChildren(father);
+            father.AddChildren(son1);
+            father.AddChildren(son2);
+            father.AddChildren(daughter1);
+            father.AddChildren(daughter2);
+            son1.Wedding(eva);
+            son1.AddChildren(kain);
+
+            grendFather.Print(2);
+            Console.ReadLine();
+        }
+    }
+}
