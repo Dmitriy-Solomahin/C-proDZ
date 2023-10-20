3fc9f55808e69859c1cd4747d6e9c5e8e1ba8597
Dmitriy Solomahin <solomahin_dima@mail.ru>
2023-10-20 19:56:10 +0300

    add Dz2-3

---
+++ Lesson2_3/Lesson2_3/Labirynth.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_3
+{
+    internal class Labirynth
+    {
+        //Доработайте приложение поиска пути в лабиринте, но на этот раз вам нужно определить сколько всего выходов имеется в лабиринте:
+        
+        
+
+        public static int HasExit(int startI, int startJ, int[,] l)
+        {
+            int exit = 0;
+            if( CheckStartPosition(startI, startJ, l))
+            {
+                var stack = new Stack<Tuple<int, int>>();
+                stack.Push(new Tuple<int,int>(startI, startJ));
+
+                while (stack.Count > 0)
+                {
+                    var pos = stack.Pop();
+                    if (l[pos.Item1, pos.Item2] == 2) exit++;
+
+                    l[pos.Item1, pos.Item2] = 1;
+
+                    if (pos.Item2 > 0 && l[pos.Item1, pos.Item2 - 1] != 1) 
+                        stack.Push(new Tuple<int, int>(pos.Item1, pos.Item2 - 1));//вверх
+                    if (pos.Item1 > 0 && l[pos.Item1 - 1, pos.Item2] != 1)
+                        stack.Push(new Tuple<int, int>(pos.Item1 - 1, pos.Item2));//влево
+                    if (pos.Item2 + 1 < l.GetLength(1) && l[pos.Item1, pos.Item2 + 1] != 1)
+                        stack.Push(new Tuple<int, int>(pos.Item1, pos.Item2 + 1));//вниз
+                    if (pos.Item1 + 1 < l.GetLength(0) && l[pos.Item1 + 1, pos.Item2] != 1)
+                        stack.Push(new Tuple<int, int>(pos.Item1 + 1, pos.Item2));//вправо
+                }
+            }
+            return exit;
+        }
+
+        private static bool CheckStartPosition(int startI, int startJ, int[,] l)
+        {
+            if (l[startI, startJ] == 0) return true;
+            else if (l[startI, startJ] == 2)
+            {
+                Console.WriteLine("вы находитесь на выходе");
+                return true;
+            }
+            Console.WriteLine("Неверная точка старта");
+            return false;
+        }
+    }
+}
---
+++ Lesson2_3/Lesson2_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] lab = new int[,]
+            {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {2, 0, 0, 0, 1, 0, 2 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 2, 1, 1, 1 }
+            };
+
+            Console.WriteLine($"Количество выходов из лабиринта с данной точки: {Labirynth.HasExit(0,0,lab)}");
+        }
+    }
+}
