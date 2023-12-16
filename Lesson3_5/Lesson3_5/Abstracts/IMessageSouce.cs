2c6199febd50af88d3803ae7e7bdcafbafbe3f9a
Dmitriy-Solomahin <116346966+Dmitriy-Solomahin@users.noreply.github.com>
2023-12-16 19:15:27 +0300

    dz 5

---
+++ Lesson3_5/Lesson3_5/Abstracts/IMessageSouce.cs
@@ -0,0 +1,17 @@
+﻿using Lesson3_5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_5.Abstracts
+{
+    public interface IMessageSource
+    {
+        Task SendAsync(NetMessage message , IPEndPoint ep);
+
+        NetMessage Receive(ref IPEndPoint ep);
+    }
+}
